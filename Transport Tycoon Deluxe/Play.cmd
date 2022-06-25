65509a2ac2f41e21f2f295eaa5c24c36267b9568
Nielany <66225830+Nielany@users.noreply.github.com>
2022-06-25 21:59:59 +0300

    Add files via upload

---
+++ Transport Tycoon Deluxe/Play.cmd
@@ -0,0 +1,3 @@
+@echo off
+echo This file is available to run the game in DOSBox without using the shortcut inside your startmenu or on your desktop.
+DOSBOX\dosbox.exe -conf R2R.conf -noconsole
---
+++ Transport Tycoon Deluxe/config.bat
@@ -0,0 +1,25 @@
+echo           ษออออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+echo           บ    Transport Tycoon Deluxe Game Information Screen     บ
+echo           ฬออออออออออออออออออออออออออออออออออออออออออออออออออออออออน
+echo           บ                Hotkeys to enjoy the game               บ
+echo           ฬออออออออออออออออออออออออออออออออออออออออออออออออออออออออน
+echo           บ  Change screen   Press [ALT]+[ENTER] at once           บ
+echo           บ  Pause game      Press [ALT]+[PAUSE] at once           บ
+echo           บ  Close game      Press [CTRL]+[F9] at once             บ
+echo           บ  Slow game down  Press [CTRL]+[F11] at once            บ
+echo           บ  Speed game up   Press [CTRL]+[F12] at once            บ
+echo           ศออออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+:NONOTES
+echo.
+pause
+:LOADGAME
+cls
+echo.
+echo  Please wait... loading the game Transport Tycoon Deluxe     
+TTDX.exe
+cls
+echo.
+echo Thank you for playing Transport Tycoon Deluxe
+echo.
+pause
+exit
